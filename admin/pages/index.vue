8022c157c3e6419c6bb7592473836a8d2fa58478
kkumarasamy <kkumarasamy@edit-place.com>
2020-05-02 13:22:18 +0530

    Products listing page | WIP

--- admin/pages/index.vue
+++ admin/pages/index.vue
@@ -1,73 +1,119 @@
 <template>
-  <div class="container">
-    <div>
-      <logo />
-      <h1 class="title">
-        admin
-      </h1>
-      <h2 class="subtitle">
-        My peachy Nuxt.js project
-      </h2>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey"
-        >
-          GitHub
-        </a>
+  <main>
+    <div class="container-fluid browsing-history mb-3">
+      <div class="row">
+        <div class="col-sm-8 col-8">
+          <h1 class="mb-3">All products</h1>
+          <!-- Buttons -->
+          <a href="" class="a-button-buy-again">Add a new product</a>
+          <a href="" class="a-button-history mr-2">Add a new category</a>
+          <a href="" class="a-button-history">Add a new owner</a>
+        </div>
       </div>
-      <a href="" class="a-button-buy-again">Buy Now</a>
     </div>
-  </div>
+    <div class="container-fluid browsing-history">
+      <div class="row">
+        <div v-for="(product) in products" :key="product._id" class="col-lg-3 col-sm-6 col-12">
+          <b-card
+            :title="product.title"
+            :img-src="product.photo != ''? product.photo : ''"
+            :img-alt="product.title"
+            img-top
+            tag="article"
+            class="mb-2 history-box img-fluid p-2"
+          >
+            <b-card-text>
+              Some quick example text to build on the card title and make up the bulk of the card's content.
+            </b-card-text>
+            <!-- Product Rating -->
+            <b-card-text>
+              <a href=""></a>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <i class="fas fa-star"></i>
+              <span class="a-letter-space"></span>
+              <span class="a-color-tertiary a-size-small asin-reviews">(1774)</span>
+            </b-card-text>
+          <!-- Product price -->
+            <b-card-text>
+              Price: <span class="text-danger">{{product.price}}</span>
+            </b-card-text>
+
+            <div class="float-right">
+              <b-button href="#" variant="primary">Update</b-button>
+              <b-button href="#" variant="primary">Delete</b-button>
+            </div>
+          </b-card>
+        </div>
+      </div>
+      <!-- <div class="row">
+        <div>
+          <b-card-group columns col-lg-3 col-sm-6 col-12>
+            <b-card
+              v-for="(product) in products" :key="product._id"
+              :title="product.title"
+              :img-src="product.photo"
+              :img-alt="product.title"
+              img-to img-fluid
+              tag="article"
+              class="mb-2 history-box"
+            >
+              <b-card-text>
+                Some quick example text to build on the card title and make up the bulk of the card's content.
+              </b-card-text>
+              <b-card-text>
+                <a href=""></a>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <i class="fas fa-star"></i>
+                <span class="a-letter-space"></span>
+                <span class="a-color-tertiary a-size-small asin-reviews">(1774)</span>
+              </b-card-text>
+              <b-card-text>
+                Price: <span class="text-danger">{{product.price}}</span>
+              </b-card-text>
+
+              <b-button href="#" variant="primary">Update</b-button>
+              <b-button href="#" variant="primary">Delete</b-button>
+            </b-card>
+
+          </b-card-group>
+        </div>
+      </div> -->
+    </div>
+  </main>
 </template>
 
 <script>
-import Logo from '~/components/Logo.vue'
 
 export default {
+  // asyncData is fetching data before nuxt page finished loading on the browser
+  // It is good for SEO because the data will be loaded first
+  async asyncData({ $axios}) {
+    // debugger
+    try {
+      let response = await $axios.$get('http://localhost:4004/api/products')
+      console.log(response.products)
+      return {
+        products: response.products
+      }
+    } catch(err) {
+
+    }
+  },
   components: {
-    Logo
+
   }
 }
 </script>
 
 <style>
-.container {
-  margin: 0 auto;
-  min-height: 100vh;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  text-align: center;
-}
-
-.title {
-  font-family: 'Quicksand', 'Source Sans Pro', -apple-system, BlinkMacSystemFont,
-    'Segoe UI', Roboto, 'Helvetica Neue', Arial, sans-serif;
-  display: block;
-  font-weight: 300;
-  font-size: 100px;
-  color: #35495e;
-  letter-spacing: 1px;
-}
-
-.subtitle {
-  font-weight: 300;
-  font-size: 42px;
-  color: #526488;
-  word-spacing: 5px;
-  padding-bottom: 15px;
-}
-
-.links {
-  padding-top: 15px;
-}
+ .card-img-top {
+   height: 200px;
+   object-fit: contain;
+ }
 </style>
