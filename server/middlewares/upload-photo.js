a632f69c670421978a6bbc6b307095ddb6494431
kkumarasamy <kkumarasamy@edit-place.com>
2020-12-07 16:25:30 +0530

    server upload-photo update

--- server/middlewares/upload-photo.js
+++ server/middlewares/upload-photo.js
@@ -13,13 +13,14 @@ const upload = multer({
     storage: multerS3({
         s3: s3,
         bucket: 'vue-amazon-clone-v2',
+        // key: 'projects/',
         acl: 'public-read',
         contentType: multerS3.AUTO_CONTENT_TYPE,
         metadata: (req, file, cb) => {
             cb(null, { fieldName: file.fieldname })
         },
         key: (req, file, cb) => {
-            cb(null, Date.now().toString())
+            cb(null, `${Date.now().toString()}`)
         }
     })
 })
